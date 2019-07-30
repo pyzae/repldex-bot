017bed83715b182753a320c940adba19f85596ef
noah feinberg <redacted427@gmail.com>
2019-07-30 19:48:37 -0400

    fixed weird stuff

--- main.js
+++ main.js
@@ -1,47 +1,55 @@
 const storage = require("./storage/storageManager");
-const discord = require('discord.js')
-const client = new discord.Client
-const config = require('./config')
-const cheerio = require('cheerio')
-require('dotenv').config()
+const discord = require("discord.js");
+const client = new discord.Client();
+const config = require("./config");
+const cheerio = require("cheerio");
+require("dotenv").config();
 
-client.on('ready', () => console.log("YES CHEF"))
+client.on("ready", () => console.log("YES CHEF"));
 
-client.on('message', msg => {
-  if(msg.channel.id == "605862116808720416"){
-    let parse = cheerio.load(msg.content)
-    let name = parse("title").text()
-    let type = parse("type").text()
-    let body = parse("description").text()
-    let tags = parse("tags").text()    
+client.on("message", msg => {
+  if (msg.author.bot) return;
 
-    let entree = new storage.Entree(
-      name,
-      type,
-      msg.author.tag,
-      body,
-      tags.split(',')
-    );
-    
-    storage.write(entree);
-  }
-  if(msg.content.startsWith("?search")){
-    let docs = storage.read(msg.content.slice(8), (docs) =>{
-      let embed = new discord.RichEmbed()
-      if(docs != "not found"){
-        embed.setTitle(docs[0].name)
-        embed.setAuthor(docs[0].author)
-        embed.addField("body: ", docs[0].body)
-        embed.addField("type: ", docs[0].type)
-        embed.addField("tags: ", docs[0].tags.join(", "))
-        embed.setFooter(docs[0]._id)
-        msg.channel.send(embed)
-      } else{
-        msg.channel.send("not found")
+  if (msg.channel.id == "605862116808720416") {
+    let parse = cheerio.load(msg.content);
+    let name = parse("title").text();
+    let type = parse("type").text();
+    let body = parse("description").text();
+    let tags = parse("tags").text();
+
+    if (name == "" && body == "") {
+      msg.reply("not enough data");
+      if (msg.deletable) {
+        msg.delete();
       }
-    })
+    } else {
+      let entree = new storage.Entree(
+        name,
+        type,
+        msg.author.tag,
+        body,
+        tags.split(",")
+      );
+
+      storage.write(entree);
+    }
   }
-})
+  if (msg.content.startsWith("?search")) {
+    let docs = storage.read(msg.content.slice(8), docs => {
+      let embed = new discord.RichEmbed();
+      if (docs != "not found") {
+        embed.setTitle(docs[0].name);
+        embed.setAuthor(docs[0].author);
+        embed.addField("body: ", docs[0].body);
+        embed.addField("type: ", docs[0].type);
+        embed.addField("tags: ", docs[0].tags.join(", "));
+        embed.setFooter(docs[0]._id);
+        msg.channel.send(embed);
+      } else {
+        msg.channel.send("not found");
+      }
+    });
+  }
+});
 
-
-client.login(process.env.TOKEN)
+client.login(process.env.TOKEN);
