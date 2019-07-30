2f3c91398fb251bb93ef3919215bad000efecef9
noah feinberg <redacted427@gmail.com>
2019-07-30 16:33:09 -0400

    writing to db works now kinda

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-var storage = require('./storage/storageManager')
+var storage = require("./storage/storageManager");
+
+//example of writing to db
+let entree = new storage.Entree({
+  name: "test",
+  type: "event",
+  author: "redacted",
+  body: "testing reading and writing",
+  tags: ["epic", "good-read"]
+});
+
+storage.write(entree);
--- storage/storageManager.js
+++ storage/storageManager.js
@@ -1,22 +1,27 @@
-var Datastore = require("nedb");
+const Datastore = require("nedb");
 
-people = new Datastore({
-  filename: __dirname + "/people.json",
+let db = new Datastore({
+  filename: __dirname + "/repldex.db",
   autoload: true
 });
 
-legends = new Datastore({
-  filename: __dirname + "/legends.json",
-  autoload: true
-});
+module.exports.Entree = class Entree {
+  constructor(name, type, author, body, tags) {
+    this.name = name;
+    this.type = type;
+    this.author = author;
+    this.body = body;
+    this.tags = tags;
+  }
+};
 
-events = new Datastore({
-  filename: __dirname + "/events.json",
-  autoload: true
-});
-
+//data should be of type Entree
 module.exports.write = function(data) {
-  legends.insert(data, err =>
+  db.insert(data, err =>
     err ? console.log("err : " + err) : console.log("succesful write")
   );
+};
+
+module.exports.read = function(name = "none", type) {
+  db.find({ name: name, type: type });
 };
